64d565e40cf12d20b7010a3a1a5b48f4b3603f80
7vma <robles23david@gmail.com>
2022-10-11 21:59:48 -0700

    fix error

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ function Header() {
     return(
         <div className="Header">
                 <h1 className="logo">WeThrift</h1>
-                <a className="login" href="">Login</a>
-                <a className="register" href="">Register</a>
+                <a className="login" href=""><h2>Login</h2></a>
+                <a className="register" href=""><h2>Register</h2></a>
         </div>
     )
 }
